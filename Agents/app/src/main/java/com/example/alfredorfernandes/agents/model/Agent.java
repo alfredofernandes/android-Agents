f4d29c1bd05650130fdf7423ece4204ca84e40fd
Alfredo Fernandes <alfredorfernandes@gmail.com>
2017-08-01 16:55:26 -0400

    - Alter Agency, Agent and Mission;

--- Agents/app/src/main/java/com/example/alfredorfernandes/agents/model/Agent.java
+++ Agents/app/src/main/java/com/example/alfredorfernandes/agents/model/Agent.java
@@ -1,8 +1,84 @@
 package com.example.alfredorfernandes.agents.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by juliana on 2017-08-01.
  */
 
-public class Agent {
+public class Agent
+{
+    private String name;
+    private int level;
+    private Agency agency;
+    private String country;
+    private String phone;
+    private String address;
+    private ArrayList<Mission> missions;
+
+    public Agent(String name, int level, Agency agency, String country, String phone, String address, ArrayList<Mission> missions) {
+        this.name = name;
+        this.level = level;
+        this.agency = agency;
+        this.country = country;
+        this.phone = phone;
+        this.address = address;
+        this.missions = missions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Agency getAgency() {
+        return agency;
+    }
+
+    public void setAgency(Agency agency) {
+        this.agency = agency;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public ArrayList<Mission> getMissions() {
+        return missions;
+    }
+
+    public void setMissions(ArrayList<Mission> missions) {
+        this.missions = missions;
+    }
 }
--- Agents/app/src/main/java/com/example/alfredorfernandes/agents/model/Mission.java
+++ Agents/app/src/main/java/com/example/alfredorfernandes/agents/model/Mission.java
@@ -1,8 +1,48 @@
 package com.example.alfredorfernandes.agents.model;
 
+import java.util.Date;
+
 /**
  * Created by juliana on 2017-08-01.
  */
 
 public class Mission {
+
+    public enum MissionStatus {
+        DONE, CANCELLED, GOING
+    }
+
+    private String missionName;
+    private Date date;
+    private MissionStatus status;
+
+    public Mission(String missionName, Date date, MissionStatus status) {
+        this.missionName = missionName;
+        this.date = date;
+        this.status = status;
+    }
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public void setMissionName(String missionName) {
+        this.missionName = missionName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public MissionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MissionStatus status) {
+        this.status = status;
+    }
 }
