33ee5bf94c0d365e86a8c4fa6cd264ed2210c198
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2017-08-03 15:24:09 -0400

    - Agency class and AgencyDAO created;

--- Agents/app/src/main/java/com/example/alfredorfernandes/agents/dao/AgencyDAO.java
+++ Agents/app/src/main/java/com/example/alfredorfernandes/agents/dao/AgencyDAO.java
@@ -1,8 +1,63 @@
 package com.example.alfredorfernandes.agents.dao;
 
-/**
- * Created by juliana on 2017-08-03.
- */
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.alfredorfernandes.agents.model.Agency;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AgencyDAO {
+public class AgencyDAO extends SQLiteOpenHelper {
+
+    public AgencyDAO(Context context) {
+        super(context, "AgentsDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sql = "CREATE TABLE Agency (id INTEGER PRIMARY KEY, name TEXT, website TEXT)";
+        sqLiteDatabase.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String sql = "DROP TABLE IF EXISTS Agency";
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
+
+    public void dbInsert(Agency agency) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues agencyData = new ContentValues();
+
+        agencyData.put("name", agency.getName());
+        agencyData.put("website", agency.getWebsite());
+
+        db.insert("Agency", null, agencyData);
+    }
+
+    public List<Agency> dbSearch() {
+
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = "SELECT * FROM Agency";
+        Cursor c = db.rawQuery(sql, null);
+
+        List<Agency> agencyList = new ArrayList<>();
+
+        while (c.moveToNext()) {
+            Agency agency = new Agency();
+
+            agency.setId(c.getLong(c.getColumnIndex("id")));
+            agency.setName(c.getString(c.getColumnIndex("name")));
+            agency.setWebsite(c.getString(c.getColumnIndex("website")));
+
+            agencyList.add(agency);
+        }
+        c.close();
+
+        return agencyList;
+    }
+
 }
