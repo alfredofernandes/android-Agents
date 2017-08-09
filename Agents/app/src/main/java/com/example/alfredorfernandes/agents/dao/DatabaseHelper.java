53d668b339b96195910e83a4ef562a730b466720
Alfredo Fernandes <alfredorfernandes@gmail.com>
2017-08-09 12:35:14 -0400

    - Add DatabaseHelper and DatabaseManager

--- Agents/app/src/main/java/com/example/alfredorfernandes/agents/dao/DatabaseHelper.java
+++ Agents/app/src/main/java/com/example/alfredorfernandes/agents/dao/DatabaseHelper.java
@@ -3,15 +3,21 @@ package com.example.alfredorfernandes.agents.dao;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-public class DatabaseHelper extends SQLiteOpenHelper {
+import com.example.alfredorfernandes.agents.model.Agency;
+import com.example.alfredorfernandes.agents.model.Agent;
+import com.example.alfredorfernandes.agents.model.Mission;
+import com.example.alfredorfernandes.agents.model.MissionAgent;
+
+public class DatabaseHelper  extends SQLiteOpenHelper {
 
     // Database Version
     private static final int DATABASE_VERSION = 1;
 
     // Database Name
     private static final String DATABASE_NAME = "AgentsDB";
-
+    private static final String TAG = DatabaseHelper.class.getSimpleName().toString();
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,25 +26,25 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        /*db.execSQL(CREATE_TABLE_AGENCY);
-        db.execSQL(CREATE_TABLE_AGENT);
-        db.execSQL(CREATE_TABLE_MISSION);
-        db.execSQL(CREATE_TABLE_MISSION_AGENT);
-
-        createData(db);*/
+        //All necessary tables you like to create will create here
+        db.execSQL(AgencyDAO.createTable());
+        db.execSQL(AgentDAO.createTable());
+        db.execSQL(MissionDAO.createTable());
+        db.execSQL(MissionAgentDAO.createTable());
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        Log.d(TAG, String.format("SQLiteDatabase.onUpgrade(%d -> %d)", oldVersion, newVersion));
+
         // on upgrade drop older tables
-        /*db.execSQL("DROP TABLE IF EXISTS " + TABLE_AGENCY);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_AGENT);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MISSION);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_MISSION_AGENT);
+        db.execSQL("DROP TABLE IF EXISTS " + Agency.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + Agent.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + Mission.TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + MissionAgent.TABLE);
 
         // create new tables
-        onCreate(db);*/
+        onCreate(db);
     }
-
 }
