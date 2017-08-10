418fe35c72322fd7e909ac3c942e9bf467dd1fa2
Alfredo Fernandes <alfredorfernandes@gmail.com>
2017-08-10 15:24:42 -0400

    - Update CreateAgentActivity added Username and Password

--- Agents/app/src/main/java/com/example/alfredorfernandes/agents/helper/AgentHelper.java
+++ Agents/app/src/main/java/com/example/alfredorfernandes/agents/helper/AgentHelper.java
@@ -16,6 +16,8 @@ public class AgentHelper {
     private final Spinner fieldCountry;
     private final EditText fieldPhoneNumber;
     private final EditText fieldAddress;
+    private final EditText fieldUsername;
+    private final EditText fieldPassword;
 
     public AgentHelper (CreateAgentActivity activity) {
 
@@ -25,10 +27,12 @@ public class AgentHelper {
         fieldCountry = (Spinner) activity.findViewById(R.id.create_agent_country);
         fieldPhoneNumber = (EditText) activity.findViewById(R.id.create_agent_phone_number);
         fieldAddress = (EditText) activity.findViewById(R.id.create_agent_address);
-
+        fieldUsername = (EditText) activity.findViewById(R.id.create_agent_username);
+        fieldPassword = (EditText) activity.findViewById(R.id.create_agent_password);
     }
 
     public Agent helperAgent() {
+
         Agent agent = new Agent();
 
         agent.setName(fieldName.getText().toString());
@@ -40,9 +44,9 @@ public class AgentHelper {
         agent.setCountry(fieldCountry.getSelectedItem().toString());
         agent.setPhone(fieldPhoneNumber.getText().toString());
         agent.setAddress(fieldAddress.getText().toString());
+        agent.setUsername(fieldUsername.getText().toString());
+        agent.setPassword(fieldPassword.getText().toString());
 
         return agent;
     }
-
-
 }
